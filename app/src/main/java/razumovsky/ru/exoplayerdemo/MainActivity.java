c77fe99576514d06f5a911c75537020424d5a92c
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-13 14:58:06 +0300

    initial commit playing single channel

---
+++ app/src/main/java/razumovsky/ru/exoplayerdemo/MainActivity.java
@@ -0,0 +1,101 @@
+package razumovsky.ru.exoplayerdemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.drm.DrmSessionManager;
+import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
+import com.google.android.exoplayer2.drm.UnsupportedDrmException;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.DebugTextViewHelper;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Uri PLAYLIST_URI = Uri.parse("http://testlivestream.rfn.ru/live/smil:m24.smil/chunklist_b1600000.m3u8");
+
+    private final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+
+    private SimpleExoPlayerView simpleExoPlayerView;
+
+    private SimpleExoPlayer player;
+    private MappingTrackSelector trackSelector;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
+        simpleExoPlayerView.requestFocus();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (player == null) {
+            initPlayer();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
+
+
+    private void initPlayer() {
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(BANDWIDTH_METER);
+        trackSelector = new DefaultTrackSelector(new Handler(), videoTrackSelectionFactory);
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, new DefaultLoadControl());
+        simpleExoPlayerView.setPlayer(player);
+        player.setPlayWhenReady(true);
+
+        DataSource.Factory mediaDataSourceFactory =
+                ((DemoApplication) getApplication()).buildDataSourceFactory(BANDWIDTH_METER);
+
+        MediaSource mediaSource =
+                new HlsMediaSource(PLAYLIST_URI, mediaDataSourceFactory, new Handler(), null);
+
+        player.prepare(mediaSource);
+
+    }
+
+
+    private void releasePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
+    }
+}

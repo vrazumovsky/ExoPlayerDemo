a20fc92929d403bd07e97e67566766c8e04afc3e
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-22 23:53:32 +0300

    exo player parse changed to custom playlist parse

--- app/src/main/java/razumovsky/ru/exoplayerdemo/MainActivity.java
+++ app/src/main/java/razumovsky/ru/exoplayerdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package razumovsky.ru.exoplayerdemo;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
@@ -18,9 +20,25 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final Uri PLAYLIST_URI = Uri.parse("http://testlivestream.rfn.ru/live/smil:m24.smil/chunklist_b1600000.m3u8");
+    private static final String INFO_TAG = "#EXTINF";
+
+
+    private static final Uri PLAYLIST_URI = Uri.parse("http://iptvsensei.ru/wp-content/uploads/2015/02/Sportivnye-kanaly.m3u");
+
+    @Override
+    public boolean releaseInstance() {
+        return super.releaseInstance();
+    }
 
     private final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
 
@@ -29,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private SimpleExoPlayer player;
     private MappingTrackSelector trackSelector;
 
-
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,18 +82,67 @@ public class MainActivity extends AppCompatActivity {
         simpleExoPlayerView.setPlayer(player);
         player.setPlayWhenReady(true);
 
-        DataSource.Factory mediaDataSourceFactory =
-                ((DemoApplication) getApplication()).buildDataSourceFactory(BANDWIDTH_METER);
+        new PlaylistLoadTask().execute("");
+    }
 
-        MediaSource mediaSource =
-                new HlsMediaSource(PLAYLIST_URI, mediaDataSourceFactory, new Handler(), null);
+    private final class PlaylistLoadTask extends AsyncTask<String, Void, Void> {
 
-        player.prepare(mediaSource);
+        @Override
+        protected Void doInBackground(String... strings) {
+            DataSource.Factory mediaDataSourceFactory =
+                    ((DemoApplication) getApplication()).buildDataSourceFactory(BANDWIDTH_METER);
+
+            MediaSource mediaSource = customParse(mediaDataSourceFactory);
+
+            player.prepare(mediaSource);
+
+            return null;
+        }
+
+        private MediaSource customParse(DataSource.Factory mediaDataSourceFactory) {
+            try {
+                List<String> channels = new ArrayList<>();
+
+                URL url = new URL(PLAYLIST_URI.toString());
+                InputStream inputStream = url.openStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (line.isEmpty()) {
+                        // Do nothing.
+                    } else if (line.startsWith(INFO_TAG)) {
+                        do {
+                            line = reader.readLine();
+                        } while (line.isEmpty());
+                        if (line.startsWith("http") && line.endsWith(".m3u8")) {
+                            channels.add(line);
+                        }
+                    }
+                }
+                return createPlaylistMediaSource(channels, mediaDataSourceFactory);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
 
     }
 
 
-    private void releasePlayer() {
+    private MediaSource createPlaylistMediaSource(List<String> channels, DataSource.Factory mediaDataSourceFactory) {
+        MediaSource[] mediaSources = new MediaSource[channels.size()];
+        for (int i = 0; i < channels.size(); i++) {
+            String url = channels.get(i);
+            MediaSource source = new HlsMediaSource(
+                    Uri.parse(url), mediaDataSourceFactory, handler, null);
+            mediaSources[i] = source;
+        }
+        return new ConcatenatingMediaSource(mediaSources);
+    }
+
+        private void releasePlayer() {
         if (player != null) {
             player.release();
             player = null;
